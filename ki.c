a6829eda729ea740de633f70f554c6f4794f9a63
Anna Olson <olsona@cs.uchicago.edu>
2014-01-25 13:04:07 -0600

    Debugging

--- ki.c
+++ ki.c
@@ -927,6 +927,7 @@ int  kiFarmerRAIphyOriginal() {
       // All scores
       double* scores = (double*)malloc(numDB*sizeof(double));
       classifySequenceAll(ki_seqs->seqs[i], db, scores);
+      printf("Classified!\n");
       for (j=0; j < numDB-1; j++) {
           sprintf(bufTop, "%6.6f,",scores[j]);
           bufTop += strlen(bufTop);
