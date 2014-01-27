967d45cc885eceffca3e9bdc20a525fd27827fde
Anna Olson <olsona@cs.uchicago.edu>
2014-01-26 19:09:20 -0600

    debug like woah

--- raiphy.c
+++ raiphy.c
@@ -400,6 +400,7 @@ void classifySequenceAll(char* seq, rai_db_t* db, double* scores) {
     index2 = 0;
 
     for (p = seq; *p != '\0'; ++p) {
+        printf("  seq = %d  ::  p = %d  ::  cur = %d\n", seq, p, (p - seq));
         if ( (p - seq) >= 70 ) { break; }
         
         index1 <<= 2; index1 |= rai_base2int[(int)*p]; index1 &= mask;
